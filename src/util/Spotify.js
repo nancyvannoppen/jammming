5dcf4466d7d9f260a21ef0bd2dced77dff65c0eb
Nancy VanNoppen <nancy@vannoppen.co>
2018-03-24 14:32:37 -0400

    adjust spotify.js getAccessToken function

--- src/util/Spotify.js
+++ src/util/Spotify.js
@@ -8,6 +8,24 @@ const Spotify = {
   getAccessToken() {
     if (accessToken) {
       return accessToken;
+    }
+    const accessTokenMatch = window.location.href.match(/access_token=([^&]*)/);
+    const expiresInMatch = window.location.href.match(/expires_in=([^&]*)/);
+    if (accessTokenMatch && expiresInMatch) {
+      accessToken = accessTokenMatch[1];
+      const expiresIn = Number(expiresInMatch[1]);
+      window.setTimeout(() => accessToken = '', expiresIn * 1000);
+      window.history.pushState('Access Token', null, '/'); // This clears the parameters, allowing us to grab a new access token when it expires.
+      return accessToken;
+    } else {
+      const accessUrl = `https://accounts.spotify.com/authorize?client_id=${clientID}&response_type=token&scope=playlist-modify-public&redirect_uri=${redirectURI}`;
+      window.location = accessUrl;
+    }
+  },
+/*
+  getAccessToken() {
+    if (accessToken) {
+      return accessToken;
     } else if (window.location.href.match('/access_token=([^&]*)/', '/expires_in=([^&]*)/')) {
       let expiresIn = 5;
       window.setTimeout(() => accessToken = '', expiresIn * 1000);
@@ -16,7 +34,7 @@ const Spotify = {
       window.location = `https://accounts.spotify.com/authorize?client_id=${clientID}&response_type=token&scope=playlist-modify-public&redirect_uri=${redirectURI}`;
     }
   },
-
+*/
   search(searchTerm) {
     const accessToken = Spotify.getAccessToken();
     return fetch(`https://api.spotify.com/v1/search?type=track&q=${searchTerm}`, {
