81238fe1a268cfd8dabf5e94b7484e1553cf3333
Nancy VanNoppen <nancy@vannoppen.co>
2018-03-24 14:26:35 -0400

    create accessToken const in spotify.js

--- src/util/Spotify.js
+++ src/util/Spotify.js
@@ -18,6 +18,7 @@ const Spotify = {
   },
 
   search(searchTerm) {
+    const accessToken = Spotify.getAccessToken();
     return fetch(`https://api.spotify.com/v1/search?type=track&q=${searchTerm}`, {
       headers: {
         Authorization: `Bearer ${accessToken}`
